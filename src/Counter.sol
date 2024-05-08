25cb40eb5583611f2a2f8c6f67a9ed7c5fbc8ac2
Alec Chen <93971719+0xAlec@users.noreply.github.com>
2024-05-08 11:50:44 -0700

    Update Counter.sol

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,14 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Counter {
-    uint256 public number;
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+contract Leaderboard is AccessControl {
+    mapping(string => uint256) public leaderboard;
+
+    function updateLeaderboard(string memory uuid, uint256 score) public {
+        leaderboard[uuid] = score;
     }
 
-    function increment() public {
-        number++;
+    function getScore(string memory uuid) public view returns (uint256) {
+        return leaderboard[uuid];
     }
 }
